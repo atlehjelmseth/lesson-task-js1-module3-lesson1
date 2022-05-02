e07a1970b58af44a3d5e261c0e2bac89ab91a5c7
atlehjelmseth <atle.hjelmseth@stud.noroff.no>
2022-05-02 14:39:23 +0200

    Update

---
+++ answer2.js
@@ -0,0 +1,5 @@
+// Question 2
+
+// platforms is a parameter with a value of 4
+// genres is a parameter with a value of action
+// publishers is a parameter with a value of 354s
---
+++ answer3.js
@@ -0,0 +1,12 @@
+// Question 3
+
+
+
+async function getCatFacts() {
+  const url = "https://noroff.herokuapp.com/v1/cat-facts/facts";
+  const response = await fetch(url);
+  const results = await response.json();
+  console.log(results.length);
+}
+
+getCatFacts()
